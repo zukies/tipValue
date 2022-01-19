42c7a64061533c2c8107a2135907fdbc36050b46
zukies <zukies28@gmail.com>
2022-01-19 11:48:02 +0200

    Deleted all files on branch 2

--- README.md
+++
@@ -1,2 +0,0 @@
-# tipValue
-This file is to create a tip calculator, if the bill is a certain amount a 15% tip is added to the total bill otherwise 20% is added
--- tipValue.js
+++
@@ -1,4 +0,0 @@
-const bill = 275
-
-const tip = bill <= 300 && bill >= 50 ? (bill * 0.15) : (bill * 0.2)
-console.log(`the bill was ${bill}, the tip was ${tip}. and the total value ${bill + tip}`)
